e94b1dad77df141057c6c151ef26cd5cfb115cdb
aharsha12 <anasharsha30@gmail.com.com>
2026-02-28 03:42:08 +0200

    chore: add README and local project changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,84 @@
 import 'package:flutter/material.dart';
-
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+  runApp( Myapp()
   );
+}
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+
+        backgroundColor: Colors.teal,
+        body:
+        SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/images/male.jpg'),
+                radius: 50,
+              ),
+              Text('Anas HS',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 25,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),),
+              Text('FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SourceSans3',
+                  fontSize: 20,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10,horizontal: 25),
+                child: ListTile(
+                  leading: Icon(Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text('+218 91 125 45 45',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSans3',
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold
+
+                    ),),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10,horizontal: 25),
+                child: ListTile(
+                  leading: Icon(Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text('anas4333@gmail.com',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSans3',
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold
+                    ),),
+                ),
+              )
+            ],
+
+          ),
+        ),
+      ),
+    );
+  }
 }
